7ff3d141f53a55cc4c90ef96b9b47413e825babc
Hansel <hansel.ramoscorp@outlook.com>
2019-02-09 14:08:02 -0600

    Inicializar atributos

--- src/main.java
+++ src/main.java
@@ -1,2 +1,10 @@
+import java.util.LinkedList;
+
 public class main {
+    LinkedList<Integer> matriz= new LinkedList<>();
+    boolean prop1, prop2, prop3;
+    public static void main(String[] args) {
+        main m=new main();
+        m.prop1 = m.prop2 = m.prop3 = false;
+    }
 }
